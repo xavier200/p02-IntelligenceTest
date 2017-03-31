de0b235ae074ef63add2c0d75eab5c5fa2de438d
Unknown <xvillanueva@mail.csuchico.edu>
2017-03-31 16:41:55 -0700

    first submission

---
+++ intelligence.js
@@ -0,0 +1,38 @@
+
+function math()
+{
+  var question
+  question= document.getElementById("Answer").value;
+
+}
+if(isNaN(question))
+{
+  alert("Enter the Answer as a number")
+}
+
+else
+{
+  question = number(question);
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
