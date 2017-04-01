6a5fc9dc6af80fb9a85bd287a13051527d5ea0aa
Unknown <xvillanueva@mail.csuchico.edu>
2017-03-31 23:42:41 -0700

    final update: test of intelligence

---
+++ intelligence.js
@@ -0,0 +1,33 @@
+
+function math()
+{
+  var question
+  question= document.getElementById("Answer").value;
+  document.getElementById("submitbutton").onclick = function () {
+          location.href = "file:///C:/Users/Xavier%20Villanueva/Documents/Cins%20110/p02-IntelligenceTest/Test2.html";
+      };
+
+
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+}
